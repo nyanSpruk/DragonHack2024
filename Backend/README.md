6254584dfeb2b4decdb279a5346b688accc7fbb7
Anja Ostovršnik <61201059+Anja159@users.noreply.github.com>
2024-04-21 02:56:39 +0200

    Dodan username api, komentarji

---
+++ Backend/README.md
@@ -0,0 +1,6 @@
+To start the the server type commands:
+1. python manage.py makemigrations
+2. python manage.py migrate
+3. python manage.py runserver
+
+The server is now running on http://127.0.0.1:8000/, the APIs can accessed via http://127.0.0.1:8000/api/...
--- Backend/projects/urls.py
+++ Backend/projects/urls.py
@@ -2,18 +2,42 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add user (username, name, role, teaches_subjects, attends_subjects)
     path('user_add/', views.UserListCreateView.as_view(), name='user-list-create'),
+
+    # Add project (user, college, subject, tags, company, title, description)
     path('project_add/', views.ProjektCreateView.as_view(), name='project-add'),
+
+    # Add subject (title, description, tags, college)
     path('subject_add/', views.PredmetCreateView.as_view(), name='predmet-add'),
+
+    # Which subjects does a professor teach?
     path('users/<int:pk>/teaches/', views.ProfessorSubjectsListView.as_view(), name='professor-subjects-list'),
+
+    # Which subjects does a student attend?
     path('users/<int:pk>/attends/', views.StudentSubjectsListView.as_view(), name='student-subjects-list'),
+
+    # Add college (name, location)
     path('colleges/', views.CollegeListView.as_view(), name='college-list'),
+
+    # Show college details
     path('colleges/<int:pk>/', views.CollegeDetailView.as_view(), name='college-detail'),
+
+    # Add tag (name)
     path('tags/', views.TagListCreateView.as_view(), name='tag-list-create'),
+
+    # Get tag details
     path('tags/<int:pk>/', views.TagDetailView.as_view(), name='tag-detail'),
+
+    # Get project details
     path('projects/<int:pk>/', views.ProjektDetailView.as_view(), name='project-detail'),
+
+    # Get projects by subject
     path('projects-by-subject/<int:predmet_id>/', views.ProjectsBySubjectListView.as_view(), name='projects-by-subject'),
-    
+
+    # Get user details based on username
+    path('users/<str:username>/', views.UserDetailUpdateView.as_view(), name='user-detail'),
+
 ]
 
 
